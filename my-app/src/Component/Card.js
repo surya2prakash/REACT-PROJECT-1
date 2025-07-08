db1fae65b23a2beca8017586c4cdd954905e781d
surya2prakash <sv251171@gmail.com>
2025-07-08 22:25:39 +0530

    Card component changes

--- my-app/src/Component/Card.js
+++ my-app/src/Component/Card.js
@@ -1,14 +1,24 @@
-import './Card.css'
+import SingleCard from "./SingleCard"
+
 
 
 function Card (props){
-    
-    return(
-      <div className="card">
-           {props.children};
-      </div>
+    return (
+        <div>
+              {/* <SingleCard  itemName={props.response[0].itemName} itemPrice={props.response[0].itemPrice}> </SingleCard>
+              <SingleCard  itemName={props.response[1].itemName} itemPrice={props.response[1].itemPrice}> </SingleCard>
+              <SingleCard  itemName={props.response[2].itemName} itemPrice={props.response[2].itemPrice}> </SingleCard>
+               <SingleCard  itemName={props.response[3].itemName} itemPrice={props.response[3].itemPrice}> </SingleCard>
+              <SingleCard  itemName={props.response[4].itemName} itemPrice={props.response[4].itemPrice}> </SingleCard>
+             <SingleCard  itemName={props.response[5].itemName} itemPrice={props.response[5].itemPrice}> </SingleCard> */}
+             {
+                props.response.map((item,i)=>(
+                   <SingleCard key={i} itemName={item.itemName} itemPrice={item.itemPrice}></SingleCard>
+                ))
+             }
+        
+        </div>
     )
-
-}
+};
 
 export default Card;
