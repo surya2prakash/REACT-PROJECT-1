0dd7c685121ca893690c30df7a1f66df11809c52
surya2prakash <sv251171@gmail.com>
2025-07-09 17:02:03 +0530

    form file added

--- my-app/src/Component/AddItems.js
+++ my-app/src/Component/AddItems.js
@@ -1,54 +1,48 @@
-import React from 'react'
-import { useState } from 'react';
-import Card from './Card';
-  export  const AddItems = () => {
-    
-    const [model,setModel] = useState("");
-    const [price,setPrice] =useState("");
+import { useState } from "react";
 
-    const [submittedItem, setSubmittedItem] = useState(null);
+import "./Additem.css"
 
-    function changeModelHandler(event){
-        setModel(event.target.value);
+function AddItems(props){
+
+    //useState --->
+
+    const[newItem,setNewItem]= useState("");
+    const[newItemPrice,setNewPrice] = useState("");
+
+    function itemNameHandler(event)
+    {
+       setNewItem(event.target.value);
     };
-     function changePriceHandler(event){
-      setPrice(event.target.value);
-       
+
+    function itemPriceHandler(event)
+   {
+       setNewPrice(event.target.value);
+    };
+
+    function submitHandler(event){
+        event.preventDefault();
+
+        const newProduct ={
+            itemName:newItem,
+            itemPrice:newItemPrice
+        }
+           console.log(newProduct);
+          props.onSaveProduct(newProduct);
+
+        setNewItem('');
+        setNewPrice('');
     }
+    return(
+      <div>
+            <form onSubmit={submitHandler}>
+                <label>Item Name:</label>
+                <input type="text" placeholder="enter Item Name" onChange={itemNameHandler} value={newItem}></input>
+                <label>Item Price</label>
+                <input type="text" placeholder="enter Item Price" onChange={itemPriceHandler} value={newItemPrice}></input>
+                <button type="submit">Submit</button>
+            </form>
+      </div>
+    )
+};
 
-function submitHandler(event) {
-    event.preventDefault();
-
-    
-    setSubmittedItem({
-      itemName: model,
-      itemPrice: price
-    });
-
-   
-    setModel("");
-    setPrice("");
-  }
-
-  return (
-    <div>
-        <form onSubmit={submitHandler} >
-            <label>Enter Mobile Brand With Model:</label>
-        <input type='text' placeholder='Enter Model Name'onChange={changeModelHandler}
-              value={model}></input>
-        <label>Enter Price:</label>
-        <input type='text' placeholder='enter price of model' onChange={changePriceHandler}
-         value={price}></input>
-        <button type='submit'> Submit</button>
-        </form>
-        {submittedItem && (
-        <Card
-          itemName={submittedItem.itemName}
-          itemPrice={submittedItem.itemPrice}
-        />
-      )}
-    </div>
-  )
-}
-
- 
+export default AddItems;
